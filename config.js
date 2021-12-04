aa9a45fe9218f7bb0e0ee582925fa39a0f732e3b
Pratyush615 <75452860+Pratyush615@users.noreply.github.com>
2021-12-04 12:00:49 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCzB4TIwOE7JMnM7tnYi2ft0iC4bNkIO6E",
+    authDomain: "spectagram-bb51d.firebaseapp.com",
+    projectId: "spectagram-bb51d",
+    storageBucket: "spectagram-bb51d.appspot.com",
+    messagingSenderId: "216701977602",
+    appId: "1:216701977602:web:94e8babf87cf60f838531b",
+};
